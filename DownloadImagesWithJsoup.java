476305343d7465eb9a19d74d6ccfa0f81fbf0200
shivam saluja <s.saluja72@gmail.com>
2023-04-09 20:20:06 +0530

    Image crawler for

--- DownloadImagesWithJsoup.java
+++ DownloadImagesWithJsoup.java
@@ -24,16 +24,27 @@ public class DownloadImagesWithJsoup {
 		Document doc;
 
 		try {
-			doc = Jsoup.connect("https://www.zomato.com/pune/bubble-bee-aundh/photos").get();	// URL from where you want to collect photos
-			Elements images = doc.select(".photos_container_load_more a img");					// Simple selector to spot all img tags in html Doc
-			for (Element image : images) {					
-				//System.out.println("src : " + image.attr("data-original").split("\\?")[0]);
-				String imgURL = image.attr("data-original").split("\\?")[0];					// Collecting data-original(or src) attribute of img tag (I have split it on ? so as to remove extra URL parameters such as fit=, crop=)
-				new File("E:/CrawlerImages").mkdirs();											// Create new directory in you system where you want to store images
-				URL url = new URL(imgURL);														
-				InputStream in = url.openConnection().getInputStream();			
-				Files.copy(in, Paths.get("E:/CrawlerImages/" + imgURL.substring(imgURL.lastIndexOf("/"))), // Save image in created folder with some name
+			doc = Jsoup.connect("http://www.xyz.com/").get();	// URL from where you want to collect photos
+
+			Elements images = doc.select("*[src~=(?i)\\.(png|jpe?g|gif)]");	// Simple selector to spot all img tags in html Doc
+			images.addAll(doc.select("*[background~=(.*)\\.(png|jpe?g|gif)]"));
+			System.out.println("src : " + images.size());
+			int count = 0;
+			for (Element image : images) {
+				System.out.println("count : " + count++);
+
+				String imgURL = image.absUrl("src").isEmpty() ? image.absUrl("background") : image.absUrl("src");                    // Collecting data-original(or src) attribute of img tag (I have split it on ? so as to remove extra URL parameters such as fit=, crop=)
+				new File("D:\\abc\\xyz\\images\\").mkdirs();                                             // Create new directory in you system where you want to store images
+
+				System.out.println("imgURL  : " + imgURL );
+
+				if (!imgURL.isEmpty()){
+					URL url = new URL(imgURL);
+					InputStream in = url.openConnection().getInputStream();
+					Files.copy(in, Paths.get("D:\\abc\\xyz\\images\\" + imgURL.substring(imgURL.lastIndexOf("/"))), // Save image in created folder with some name
 						StandardCopyOption.REPLACE_EXISTING);
+				}
+
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
